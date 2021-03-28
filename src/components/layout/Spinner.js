afbf7aa2fd9ef9bf84c88037fdcf8007bc83efab
sporodreki <mohamed.mardhi@gmail.com>
2021-03-28 22:37:20 +0200

    adding Search and Alert Components

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,13 +1,11 @@
 import React, {Fragment} from 'react';
 import spinner from './spinner.gif';
 
-const Spinner = () => {
-
+const Spinner = () => {    
         return (
             <Fragment>
                 <img src={spinner} alt="" style= {{width: '200px', margin: 'auto', dispalay: 'block'}} />
             </Fragment>
-
         )
 }
 
