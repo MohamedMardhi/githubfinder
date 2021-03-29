cbe61e1be2feed3f4cca5f98b0ee83d01672efef
sporodreki <mohamed.mardhi@gmail.com>
2021-03-29 20:46:23 +0200

    adding Router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 
  const Navbar = ({icon, title}) => {
@@ -10,6 +11,14 @@ import PropTypes from 'prop-types';
                 <i className={icon}></i>
                 {title}
                 </h1>
+                <ul>
+                    <li>    
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>    
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
             </nav>
         )    
 }
